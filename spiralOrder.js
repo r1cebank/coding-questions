a804ee7312fc3082ca0f2254d37d6adb8a4788d9
r1cebank <siyuangao@gmail.com>
2017-11-16 21:50:47 -0800

    adding spiral

---
+++ spiralOrder.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+
+function zip(rows) {
+    return rows[0].map(function(_,c) {
+        return rows.map(function(row) {
+            return row[c];
+        });
+    }).reverse();
+};
+var spiralOrder = function(matrix) {
+    if (matrix.length < 1) { return matrix; }
+    return matrix.splice(0, 1)[0].concat(matrix.length > 0 ? spiralOrder(zip(matrix)) : []);
+};
+
+
+console.log(spiralOrder([]));
